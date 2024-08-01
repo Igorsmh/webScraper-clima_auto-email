8eaa9c04637e8fe58038b4799d51c70c9cd5d506
Igor <shenriques@ihm.com.br>
2024-08-01 17:32:32 -0300

    add-func max_min/condicao

--- WebScraper_Previsão_Tempo.py
+++ WebScraper_Previsão_Tempo.py
@@ -8,33 +8,43 @@ from src import init_driver
 driver = init_driver()
 driver.get("https://www.tempo.com/rio-de-janeiro_rio-de-janeiro-l12987.htm")
 sleep(3)
+
 #Extrair a temperatura atual.
 temp = driver.find_element(By.XPATH, "//span[@class='dato-temperatura changeUnitT']")
 temperatura = temp.text
+
 #Extrair a condição do tempo atual (ex. ensolarado, nublado, etc.).
 cond_temp = driver.find_elements(By.XPATH,'//span[@class= "descripcion"]')
 cond_temp = cond_temp[0].text
+
 #Extrair a previsão para os próximos 3 dias (temperatura e condição do tempo)
 
-# temperatura
-amanha_temp = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d2']/span/span")
-dep_amanha_temp = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d3']/span/span")
-dep_dep_amanha_temp = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d4']/span/span")
+def max_min(dia):
 
-a_t = amanha_temp[3].text
-d_a_t = dep_amanha_temp[3].text
-d_d_a_t = dep_dep_amanha_temp[3].text
+    elementos = driver.find_elements(By.XPATH, f"//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d{dia}']/span/span")
+    max_min = elementos[3].text
+    return max_min
 
-# Condição do clima
-amanha_cond = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d2']/span/img")
-dep_amanha_cond = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d3']/span/img")
-dep_dep_amanha_cond = driver.find_elements(By.XPATH, "//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d4']/span/img")
+def condicao(dia):
 
-a_c = amanha_cond[0].get_attribute('alt')
-d_a_c = dep_amanha_cond[0].get_attribute('alt')
-d_d_a_c = dep_dep_amanha_cond[0].get_attribute('alt')
+    elementos = driver.find_elements(By.XPATH, f"//ul[@class='grid-container-7 dias_w']/li[@class='grid-item dia d{dia}']/span/img")
+    condicao = elementos[0].get_attribute('alt')
+    return condicao
+
+
+a_t = max_min(2)
+d_a_t = max_min(3)
+d_d_a_t = max_min(4)
+
+
+a_c = condicao(2)
+d_a_c = condicao(3)
+d_d_a_c = condicao(4)
+
 
 print(f"Amanha: {a_t} {a_c}  \n Depois de amanha:  {d_a_t} {d_a_c}  \n Depois de depois de amanha: {d_d_a_t} {d_d_a_c} " )
+
+
 
 input('')
 driver.close()
